f757c8bc3704f41c8c1ff3e8cdcb28f658f82b05
prajwals15 <prajwals15@gmail.com>
2023-05-01 15:42:16 +0200

    update structure

---
+++ functions/hello.py
@@ -0,0 +1,10 @@
+
+def hello(name):
+    print('Hello ' + name)
+    print("Length of your name is " + str(len(name)))
+    return "Welcome to Python Programming " + name.upper()
+
+
+print(hello("Praj"))
+print(hello("Ram"))
+print(hello("Neg"))
--- myIntroduction.py
+++
@@ -1,7 +0,0 @@
-print ('Hello World')
-print ("What is your name?")
-myName = input()
-print ("Hello " + myName);
-print ("What is your age?");
-myAge = input()
-print ("Your age after 5 years would be " + str(int(myAge)+5));
